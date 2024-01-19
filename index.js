b645ba080a24d10321bf85ca17e76a5d9b7963fd
Rohit D <rohitdhawale07@gmail.com>
2024-01-19 19:13:28 +0530

    Data Fetched Successfully

--- index.js
+++ index.js
@@ -1,57 +1,79 @@
-const apiKey = "c16181903fmsh1033d4b795f17b4p107622jsnfd086ec0495b";
-const baseUrl = "https://weatherapi-com.p.rapidapi.com/forecast.json";
+const apiKey = `b3c2b951c74116383a59e56d53f0e644`;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const locationInput = document.getElementById("locationInput");
-  const searchButton = document.getElementById("searchButton");
-  const temperature = document.getElementById("temperature");
-  const humidity = document.getElementById("humidity");
-  const windSpeed = document.getElementById("windSpeed");
-  const conditions = document.getElementById("conditions");
-  const dropdown = document.getElementById("dropdown");
-  const dropdown2 = document.getElementById("dropdown2");
+async function fetchWeatherData(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    );
 
-  // Function to fetch weather data
-  async function fetchWeatherData(location) {
-    const url = `${baseUrl}?q=${location}&days=3`;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": apiKey,
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-      },
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data);
-      temperature.textContent = data.current.feelslike_f;
-      humidity.textContent = data.current.humidity;
-      windSpeed.textContent = data.current.wind_kph;
-      conditions.textContent = data.current.condition.text;
-      const x = data.forecast.forecastday[0].hour;
-      for(const hour of x){
-        const y = document.createElement("option")
-        y.textContent = `${hour.time.slice(11)} ${hour.temp_c}`
-        dropdown.appendChild(y);
-      }
-      //avgtemp_c
-      const a = data.forecast.forecastday.map((ele)=>{
-        const d = document.createElement("option");
-        d.textContent = `${ele.date} ${ele.day.avgtemp_c}`;
-        dropdown2.appendChild(d);
-      })
-      
-        
-    } catch (error) {
-      console.error(error);
-      alert(error.error.message);
+    if (!response.ok) {
+      throw new Error("Unable to fetch weather data");
     }
+    const data = await response.json();
+    console.log(data);
+    console.log(data.main.temp);
+    console.log(data.name);
+    console.log(data.wind.speed);
+    console.log(data.main.humidity);
+    console.log(data.visibility);
+    updateWeatherUI(data);
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  searchButton.addEventListener("click", function () {
-    const location = locationInput.value;
-    fetchWeatherData(location);
-  });
+const cityElement = document.querySelector(".city");
+const temperature = document.querySelector(".temp");
+const windSpeed = document.querySelector(".wind-speed");
+const humidity = document.querySelector(".humidity");
+const visibility = document.querySelector(".visibility-distance");
+
+const descriptionText = document.querySelector(".description-text");
+const date = document.querySelector(".date");
+const descriptionIcon = document.querySelector(".description i");
+
+// fetchWeatherData();
+
+function updateWeatherUI(data) {
+  cityElement.textContent = data.name;
+  temperature.textContent = `${Math.round(data.main.temp)}`;
+  windSpeed.textContent = `${data.wind.speed} km/h`;
+  humidity.textContent = `${data.main.humidity}%`;
+  visibility.textContent = `${data.visibility / 1000} km`;
+  descriptionText.textContent = data.weather[0].description;
+
+  const currentDate = new Date();
+  date.textContent = currentDate.toDateString();
+  const weatherIconName = getWeatherIconName(data.weather[0].main);
+  descriptionIcon.innerHTML = `<i class="material-icons">${weatherIconName}</i>`;
+}
+
+const formElement = document.querySelector(".search-form");
+const inputElement = document.querySelector(".city-input");
+
+formElement.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const city = inputElement.value;
+  if (city !== "") {
+    fetchWeatherData(city);
+    inputElement.value = "";
+  }
 });
+
+function getWeatherIconName(weatherCondition) {
+  const iconMap = {
+    Clear: "wb_sunny",
+    Clouds: "wb_cloudy",
+    Rain: "umbrella",
+    Thunderstorm: "flash_on",
+    Drizzle: "grain",
+    Snow: "ac_unit",
+    Mist: "cloud",
+    Smoke: "cloud",
+    Haze: "cloud",
+    Fog: "cloud",
+  };
+
+  return iconMap[weatherCondition] || "help";
+}
